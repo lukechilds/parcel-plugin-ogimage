a9ae5773a624a2b3e363b27b2e897c144c972add
JPeer264 <jan.oster94@gmail.com>
2019-08-15 10:41:38 +0200

    Fix: do not fail and exit properly on errors

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ const ora = require('ora');
 const chalk = require('chalk');
 const prettyMs = require('pretty-ms');
 
-const getMetaTag = (html, property) => {
+const getMetaTag = (html, property, spinner) => {
 	const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
+	const executedRegex = regex.exec(html);
 
-	return regex.exec(html)[0];
+	if (!executedRegex) {
+		spinner.fail(`Missing ${property}`);
+
+		return;
+	}
+
+	return executedRegex[0];
 };
 
-const getMetaTagContent = metaTagHtml => {
+const getMetaTagContent = (metaTagHtml, property, spinner) => {
 	const regex = /content=["]([^"]*)["]/i;
+	const executedRegex = regex.exec(metaTagHtml);
 
-	return regex.exec(metaTagHtml)[1];
+	if (!executedRegex) {
+		spinner.fail(`Missing content attribute in ${property}`);
+
+		return;
+	}
+
+	return executedRegex[1];
 };
 
 module.exports = bundler => {
@@ -22,29 +36,38 @@ module.exports = bundler => {
 		if (process.env.NODE_ENV !== 'production') {
 			return;
 		}
-		console.log('');
+
 		const spinner = ora(chalk.grey('Fixing og:image link')).start();
 		const start = Date.now();
 
 		const htmlPath = path.join(bundler.options.outDir, 'index.html');
 		const html = fs.readFileSync(htmlPath).toString();
 
-		const ogImageTag = getMetaTag(html, 'og:image');
-		const ogImageContent = getMetaTagContent(ogImageTag);
+		const ogImageTag = getMetaTag(html, 'og:image', spinner);
+		const ogImageContent = getMetaTagContent(ogImageTag, 'og:image', spinner);
 
-		const ogUrlTag = getMetaTag(html, 'og:url');
-		const ogUrlContent = getMetaTagContent(ogUrlTag);
+		const ogUrlTag = getMetaTag(html, 'og:url', spinner);
+		const ogUrlContent = getMetaTagContent(ogUrlTag, 'og:url', spinner);
 
-		const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
-		const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
-		const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
+		const hasErrors = !ogUrlTag || !ogUrlContent || !ogImageTag || !ogImageContent;
 
-		fs.writeFileSync(htmlPath, patchedHtml);
+		if (!hasErrors) {
+			const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
+			const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
+			const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
+
+			fs.writeFileSync(htmlPath, patchedHtml);
+		}
 
 		const end = Date.now();
+		const symbol = hasErrors ? chalk.red('✖') : '✨ ';
+		const text = hasErrors
+			? chalk.red('Failed to fix og:image link. Please fix the above errors.')
+			: chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`);
+
 		spinner.stopAndPersist({
-			symbol: '✨ ',
-			text: chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`)
+			symbol,
+			text,
 		});
 	});
 };
