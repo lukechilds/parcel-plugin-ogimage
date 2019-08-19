466b0b31a6c7a1b02e6bcc2fbb3e7db11b78537e
JPeer264 <jan.oster94@gmail.com>
2019-08-19 17:17:52 +0200

    Feat: handle error within a try/catch

--- index.js
+++ index.js
@@ -5,27 +5,23 @@ const ora = require('ora');
 const chalk = require('chalk');
 const prettyMs = require('pretty-ms');
 
-const getMetaTag = (html, property, spinner) => {
+const getMetaTag = (html, property) => {
 	const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
 	const executedRegex = regex.exec(html);
 
 	if (!executedRegex) {
-		spinner.fail(`Missing ${property}`);
-
-		return;
+		throw new Error(`Missing ${property}`);
 	}
 
 	return executedRegex[0];
 };
 
-const getMetaTagContent = (metaTagHtml, property, spinner) => {
+const getMetaTagContent = (metaTagHtml, property) => {
 	const regex = /content=["]([^"]*)["]/i;
 	const executedRegex = regex.exec(metaTagHtml);
 
 	if (!executedRegex) {
-		spinner.fail(`Missing content attribute in ${property}`);
-
-		return;
+		throw new Error(`Missing content attribute in ${property}`);
 	}
 
 	return executedRegex[1];
@@ -36,38 +32,42 @@ module.exports = bundler => {
 		if (process.env.NODE_ENV !== 'production') {
 			return;
 		}
+		console.log('');
 
 		const spinner = ora(chalk.grey('Fixing og:image link')).start();
 		const start = Date.now();
 
 		const htmlPath = path.join(bundler.options.outDir, 'index.html');
 		const html = fs.readFileSync(htmlPath).toString();
+		let hasErrors = false;
 
-		const ogImageTag = getMetaTag(html, 'og:image', spinner);
-		const ogImageContent = getMetaTagContent(ogImageTag, 'og:image', spinner);
+		try {
+			const ogImageTag = getMetaTag(html, 'og:image');
+			const ogImageContent = getMetaTagContent(ogImageTag, 'og:image');
 
-		const ogUrlTag = getMetaTag(html, 'og:url', spinner);
-		const ogUrlContent = getMetaTagContent(ogUrlTag, 'og:url', spinner);
+			const ogUrlTag = getMetaTag(html, 'og:url');
+			const ogUrlContent = getMetaTagContent(ogUrlTag, 'og:url');
 
-		const hasErrors = !ogUrlTag || !ogUrlContent || !ogImageTag || !ogImageContent;
-
-		if (!hasErrors) {
 			const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
 			const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
 			const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
 
 			fs.writeFileSync(htmlPath, patchedHtml);
+		} catch (error) {
+			spinner.fail(error.message);
+
+			hasErrors = true;
 		}
 
 		const end = Date.now();
 		const symbol = hasErrors ? chalk.red('✖') : '✨ ';
-		const text = hasErrors
-			? chalk.red('Failed to fix og:image link. Please fix the above errors.')
-			: chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`);
+		const text = hasErrors ?
+			chalk.red('Failed to fix og:image link. Please fix the above error.') :
+			chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`);
 
 		spinner.stopAndPersist({
 			symbol,
-			text,
+			text
 		});
 	});
 };
