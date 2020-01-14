156025ae938b511e8ab6faf1e8c6a8c0bb8726f2
Benjamin Hollway <benjamin@nothingrandom.com>
2020-01-14 14:29:23 +0100

    Fix for tests

--- index.js
+++ index.js
@@ -7,43 +7,44 @@ const prettyMs = require('pretty-ms');
 const glob = require('glob');
 
 const getMetaTag = (html, property) => {
-  const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
+	const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
 
-  return regex.exec(html)[0];
+	return regex.exec(html)[0];
 };
 
-const getMetaTagContent = (metaTagHtml) => {
-  const regex = /content=["]([^"]*)["]/i;
+const getMetaTagContent = metaTagHtml => {
+	const regex = /content=["]([^"]*)["]/i;
 
-  return regex.exec(metaTagHtml)[1];
+	return regex.exec(metaTagHtml)[1];
 };
 
-module.exports = (bundler) => {
-  bundler.on('buildEnd', async () => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    console.log('');
-    const spinner = ora(chalk.grey('Fixing og:image link')).start();
-    const start = Date.now();
+module.exports = bundler => {
+	bundler.on('buildEnd', async () => {
+		if (process.env.NODE_ENV !== 'production') {
+			return;
+		}
+		console.log('');
+		const spinner = ora(chalk.grey('Fixing og:image link')).start();
+		const start = Date.now();
 
-    glob.sync(`${bundler.options.outDir}/**/index.html`).forEach(function(file) {
-      const htmlPath = path.resolve(file);
-      const html = fs.readFileSync(htmlPath).toString();
-      const ogImageTag = getMetaTag(html, 'og:image');
-      const ogImageContent = getMetaTagContent(ogImageTag);
-      const ogUrlTag = getMetaTag(html, 'og:url');
-      const ogUrlContent = getMetaTagContent(ogUrlTag);
-      const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
-      const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
-      const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
-      fs.writeFileSync(htmlPath, patchedHtml);
-    });
+		glob.sync(`${bundler.options.outDir}/**/*.html`).forEach(file => {
+			const htmlPath = path.resolve(file);
+			const html = fs.readFileSync(htmlPath).toString();
+			const ogImageTag = getMetaTag(html, 'og:image');
+			const ogImageContent = getMetaTagContent(ogImageTag);
+			const ogUrlTag = getMetaTag(html, 'og:url');
+			const ogUrlContent = getMetaTagContent(ogUrlTag);
+			const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
+			const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
+			const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl);
 
-    const end = Date.now();
-    spinner.stopAndPersist({
-      symbol: '✨ ',
-      text: chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`)
-    });
-  });
+			fs.writeFileSync(htmlPath, patchedHtml);
+		});
+
+		const end = Date.now();
+		spinner.stopAndPersist({
+			symbol: '✨ ',
+			text: chalk.green(`Fixed og:image link in ${prettyMs(end - start)}.`)
+		});
+	});
 };
